b950ed6b568dd3717862deacfbc731fac2137e6d
Chen Huang <toocold@foxmail.com>
2024-03-13 11:24:18 +0800

    [feat] fitting for future vehicle per capita

---
+++ src/task_ev_fit.py
@@ -0,0 +1,59 @@
+import numpy as np
+import polars as pl
+from scipy.optimize import curve_fit
+
+# 步骤1: 定义模型函数
+def model(pgdp, alpha, beta, s):
+    return s * np.exp(alpha * np.exp(beta * pgdp))
+
+
+data = pl.scan_parquet('../data/data_task/gdp_vehicle.parquet')
+r10_list = pl.read_csv('../data/data_man/r10_list.csv')
+
+data_concat = []
+# 步骤2: 拟合模型
+# 这里假设你已经有了实际的PGDP数据(pgdp_data)和对应的x{i, t}数据(x_data)
+for k in r10_list['region'].to_list():
+    data_hist = (
+        data.filter(
+        (pl.col('model')=='historical') &
+        (pl.col('scenario')=='historical') &
+        (pl.col('region')==k))
+        .sort('year')
+        .collect()
+    )
+
+    saturation = data_hist.select(pl.col('saturation')).to_numpy().ravel()[0] 
+    pgdp_data = data_hist.select(pl.col('gdp_per_cap')).to_numpy().ravel() # 实际的PGDP数据
+    pvehicle_data = data_hist.select(pl.col('vehicle_per_cap')).to_numpy().ravel()  # 对应的x{i, t}数据
+    alpha = -5.58
+
+    # 使用curve_fit拟合模型
+    params, cov = curve_fit(lambda pgdp, beta: model(pgdp, alpha, beta, saturation),
+                             pgdp_data, pvehicle_data, bounds=([-np.inf], [0]))
+
+    # 打印拟合得到的参数
+    beta = params[0]
+    print("拟合参数: beta =", params[0])
+    data_hist = (
+        data_hist.with_columns(alpha=alpha)
+        .with_columns(beta=beta)
+        .lazy()
+    )
+    data_concat.append(data_hist)
+# 步骤3: 进行预测
+# 假设你有未来的PGDP数据future_pgdp_data
+    for i,j in data.filter(pl.col('model')!='historical').select(pl.col('model','scenario')).unique().collect().rows():
+        data_forecast = (
+            data.filter(pl.col('model')==i)
+            .filter(pl.col('scenario')==j)
+            .filter(pl.col('region')==k)
+            .filter(pl.col('year')>=2021)
+            .sort('year')
+            .with_columns(vehicle_per_cap=model(pl.col('gdp_per_cap'), alpha, beta, saturation))
+            .with_columns(alpha=alpha, beta=beta)
+            )
+        data_concat.append(data_forecast)
+# 步骤4：合并
+data_concat = pl.concat(data_concat)
+data_concat.sink_parquet('../data/data_task/gdp_vehicle_fitting.parquet')
--- src/task_ev_merge.py
+++ src/task_ev_merge.py
@@ -7,22 +7,26 @@ gdp_vehicle = (
     .filter(pl.col('year')<=2020)  # owing to no vehicle data in 2021, but gdp data exists
     .with_columns(model=pl.lit('historical'))
     .with_columns(scenario=pl.lit('historical'))
-    .with_columns(unit=pl.lit('US$(2010)'))
     .select(pl.col('model','scenario','region','year','gdp_per_cap','unit'))
     .vstack(
         pl.read_parquet('../data/data_import/gdp_per_cap_future.parquet')
     )
+    .with_columns(gdp_per_cap=pl.col('gdp_per_cap') / 10000) # dollar to 10^4 dollars
+    .with_columns(unit=pl.lit('10^4 US$(2010)'))
     .join(
         pl.read_csv('../data/data_task/vehicle_per_capita_r10.csv'),
         on=['region','year'],
         how='left',
         suffix='_vehicle'
     )
+    .with_columns(vehicle_per_cap=pl.col('vehicle_per_cap') / 1000) # vehicle per 1000 to vehicle per cap
+    .with_columns(unit_vehicle=pl.lit('vehicle_per_capita'))
     .join(
         pl.read_csv('../data/data_man/vehicle_saturation.csv'),
         on=['region'],
         how='left'
     )
+    .with_columns(saturation=pl.col('saturation') / 1000)  # vehicle per 1000 to vehicle per cap
     .filter(pl.col('year')>=2005)  # earliest vehicle data from 2005
 )    
 
